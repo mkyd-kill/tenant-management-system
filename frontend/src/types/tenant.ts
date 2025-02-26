0800df1c667bfe60d6b10b5a5a254496d0bfd043
Romeo Namunde <romeomureithi@gmail.com>
2025-02-26 16:21:39 +0300

    tenant interface

---
+++ frontend/src/types/tenant.ts
@@ -0,0 +1,9 @@
+export interface Tenant {
+    id: number;
+    name: string;
+    email: string;
+    phone?: string;
+    move_in_date: string;
+    is_active: boolean;
+    created_at: string;
+}
