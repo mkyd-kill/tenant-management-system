a045c715b26dc9fa79f09f394e05fb45cc508608
Romeo Namunde <romeomureithi@gmail.com>
2025-03-02 16:53:21 +0300

    registered custom model

--- backend/tenant_management/settings.py
+++ backend/tenant_management/settings.py
@@ -144,3 +144,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom user model
+AUTH_USER_MODEL = 'tenant_app.Tenant'
