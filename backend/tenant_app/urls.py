625a05ce6f88a581432c2e3e1bbc5b478e7ff359
Romeo Namunde <romeomureithi@gmail.com>
2025-03-02 16:52:50 +0300

    added a tenant register route, serializer and url

--- backend/tenant_app/urls.py
+++ backend/tenant_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TenantViewSet
+from .views import TenantViewSet, TenantRegisterViewSet
 
 router = DefaultRouter()
 router.register(r'tenants', TenantViewSet)
+router.register(r'register-tenant', TenantRegisterViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/tenant_app/views.py
+++ backend/tenant_app/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Tenant
-from .serializers import TenantSerializer
+from .serializers import TenantSerializer, TenantFormSerializer
 
 class TenantViewSet(viewsets.ModelViewSet):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
     permission_classes = [IsAuthenticated]
+
+class TenantRegisterViewSet(viewsets.ModelViewSet):
+    serializer_class = TenantFormSerializer
+    queryset = Tenant.objects.all()
+    permission_classes = [AllowAny]
