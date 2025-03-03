c6bee0d96799d12f651b776ffb0e71a87bc41fbb
Romeo Namunde <romeomureithi@gmail.com>
2025-03-03 17:39:27 +0300

    changed tenant,property view urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import PropertyViewSet, TenantViewSet
 
 router = DefaultRouter()
-router.register(r'properties', PropertyViewSet.as_view, basename='property')
-router.register(r'tenants', TenantViewSet.as_view, basename='tenants')
+router.register(r'properties', PropertyViewSet, basename='property')
+router.register(r'tenants', TenantViewSet, basename='tenant')
 
 urlpatterns = [
     path('', include(router.urls)),
