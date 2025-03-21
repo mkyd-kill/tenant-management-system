f9d317f76c5caa85e3961023389f2927e5acfb94
Romeo Mureithi <romeomureithi@gmail.com>
2025-03-21 05:44:19 +0300

    solved redux error

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
+  reducer: {
+    auth: authReducer,
+    // Add propertySlice and tenantSlice later
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
