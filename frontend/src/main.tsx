ad22c064a1183358f0f8ad27a64973da9a868f47
Romeo Mureithi <romeomureithi@gmail.com>
2025-03-24 18:38:32 +0300

    deleted base style

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'; // Import the Provider
 import { store } from './redux/store'; // Import the Redux store
 import App from './App';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
