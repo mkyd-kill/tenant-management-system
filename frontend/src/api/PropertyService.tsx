2261ea44c13118b7b155ff3cf43de21a59b9277f
Romeo Mureithi <romeomureithi@gmail.com>
2025-03-30 07:19:13 +0300

    created property api

---
+++ frontend/src/api/PropertyService.tsx
@@ -0,0 +1,50 @@
+import { api } from "./_base";
+import { handleError } from "../helpers/ErrorHanlder";
+import { PropertyGet, PropertyPost, Facility } from "../models/Property";
+
+const baseUrl = `${api}/properties`;
+
+export const propertyAddAPI = async () => {
+    try {
+        const data = await api.post<PropertyPost>(baseUrl);
+        return data;      
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const propertyGetOneAPI = async (id: number) => {
+    try {
+        const data = await api.get<PropertyGet>(`${baseUrl}/${id}`);
+        return data;  
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const propertyGetAPI = async () => {
+    try {
+        const data = await api.get<PropertyGet[]>(baseUrl);
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const propertyDeleteAPI = async (id: number) => {
+    try {
+        const data = await api.delete<PropertyGet>(`${baseUrl}/${id}`);
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const facilityAddAPI = async () => {
+    try {
+        const data = await api.post<Facility>(baseUrl + 'facilities');
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
