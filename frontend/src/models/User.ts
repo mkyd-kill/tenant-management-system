7a1a072ca0ccb4a0f53f03164c4dcc6c1f570af4
Romeo Mureithi <romeomureithi@gmail.com>
2025-03-31 22:31:07 +0300

    updated user model

--- frontend/src/models/User.ts
+++ frontend/src/models/User.ts
@@ -1,10 +1,10 @@
 export type UserProfileToken = {
-    email: string,
-    password: string,
+    userName: string;
+    email: string;
     token: string;
 };
 
 export type UserProfile = {
-    username: string,
-    email: string,
+    userName: string;
+    email: string;
 };
