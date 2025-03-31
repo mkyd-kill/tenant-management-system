c3daaa42142dd6cdacc231a5de47f6a6a0e01490
Romeo Mureithi <romeomureithi@gmail.com>
2025-03-31 22:31:29 +0300

    updated base api

--- frontend/src/api/_base.ts
+++ frontend/src/api/_base.ts
@@ -8,10 +8,3 @@ export const api = axios.create({
         'Content-Type': 'application/json'
     }
 });
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('accessToken');
-    
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-})
