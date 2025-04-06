05615c3a93fd0ce9dd0e186093b12f89558b95b4
Romeo Mureithi <romeomureithi@gmail.com>
2025-04-06 23:49:21 +0300

    dashboard init

---
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+
+interface Props {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <>
+      <h2>You logged in successfull</h2>
+    </>
+  );
+};
+
+export default Dashboard;
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import ProtectedRoute from "./ProtectedRoute";
+import Dashboard from "../components/dashboard/Dashboard";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,13 @@ export const router = createBrowserRouter([
         children: [
             { path: "login", element: <LoginPage /> },
             { path: "register", element: <RegisterPage /> },
+            { path: "dashboard",
+                element: (
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                )
+            }
         ],
     },
 ]);
