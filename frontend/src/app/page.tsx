e4488b909fc3360f5bb42d22d0b75c7d9dd699d0
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-16 20:28:21 +0300

    added footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,10 @@
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
-    <h2>Welcome. This is the Homepage</h2>
+    <>
+      <h2>Welcome. This is the Homepage</h2>
+      <Footer />
+    </>
   );
 }
---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+      <div
+        className="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 px-1 pb-2 bg-white rounded-xl"
+      >
+        <span className="flex flex-grow text-[#475467] font-semibold text-xs text-center pt-2.5">
+          Developed by Romeo Namunde Mureithi
+        </span>
+      </div>
+    );
+  }
+  export default Footer;
