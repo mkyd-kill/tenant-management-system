412e2263701fd8bf89465390481eb8b60fea8ae2
Romeo Mureithi <romeomureithi@gmail.com>
2025-06-25 01:50:11 +0300

    unittest

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Property, Tenant, Staff, User
 
-# Create your tests here.
+class LandlordCRUDTest(TestCase):
+    def setUp(self):
+        self.landlord = User.objects.create_user("landlord2", "landlord2@gmail.com", "pass1234", is_landlord=True)
+
+    def test_create_property(self):
+        prop = Property.objects.create(
+            landlord=self.landlord, name="Sunset Villa", address="123 Main St", rooms=10, floors=2 
+        )
+        self.assertEqual(Property.objects.count(), 1)
+        self.assertEqual(prop.name, 'Sunset Villa')
+
+    def test_update_property(self):
+        prop = Property.objects.create(landlord=self.landlord, name="Sunset Villa", address="123 Main St", rooms=10, floors=2)
+        prop.name = "Sunset Plaza"
+        prop.save()
+        self.assertEqual(Property.objects.first().name, 'Sunset Villa')
