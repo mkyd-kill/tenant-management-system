28c0c579acbd92407712c084b53b2a1912f80079
Romeo Mureithi <romeomureithi@gmail.com>
2025-08-01 21:21:28 +0300

    navbar navigation

--- frontend/src/components/homepage/navbar/Navbar.tsx
+++ frontend/src/components/homepage/navbar/Navbar.tsx
@@ -25,13 +25,13 @@ const Navbar = () => {
           </span>
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <Link href={ path.includes("signup") ? "/home" : "/signup"}>
+          <Link href={ path.includes("signup") || path.includes("signin") ? "/home" : "/signup"}>
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               name="get started"
             >
-              { path.includes("signup") ? "Return Home" : "Get Started"}
+              { path.includes("signup") || path.includes("signin") ? "Return Home" : "Get Started"}
             </button>
           </Link>
         </div>
